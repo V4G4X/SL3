4bc3203da9e33afcc8a6e87dec769b4f81e2a61f
V4G4X <varun.gawande@gmail.com>
2019-09-09 18:57:41 +0530

    Tweaked Around for WebDev in new System

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
-// alert("LALALALALA");
-
 function validate() {
     var fname = document.forms["personalDetails"]["fName"];
     if (fname.value===""){
-        alert("Please Enter First Name");
+        fname.value = prompt("Please Enter First Name");
         return false;
     }
 
     var lname = document.forms["personalDetails"]["lName"];
     if(lname.value===""){
-        alert("Please Enter Last Name");
+        lname.value = prompt("Please Enter Last Name");
         return false;
     }
 
+    var email = document.forms["personalDetails"]["emailId"];
+    if(email.value===""){
+        email.value = prompt("Please Enter Email");
+        for (let index = 0; index < email.value; index++) {
+            let element = email.value[index];
+            if (element==='@') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    let date = document.forms["personalDetails"]["dob"];
+    if (date.value==="") {
+        alert("Please Enter your Date of Birth");
+        return false;
+    }
+
+    let num = document.forms["personalDetails"]["number"].value;
+    if(num===""){
+        alert("Please Enter your Phone Number");
+        return false;
+    }
 }
